50d553510000bee9861ae548fb6acd427d7f99c2
dnond <dnond@@users.noreply.github.com>
2022-07-30 15:54:43 +0900

    imple pre notify

--- lib/mob_timer.rb
+++ lib/mob_timer.rb
@@ -2,32 +2,50 @@ require 'logger'
 require_relative './notifier'
 
 class MobTimer
-  def start(mob_members:, interval: 20 * 60)
+  PRE_NOTIFY_SEC = 2 * 60
+
+  def start(mob_members:, interval: 20 * 60, randomize: false, pre_notify: PRE_NOTIFY_SEC)
     logger.info('START mobtimer!')
     logger.info("members: #{mob_members.join}")
     logger.info("interval: #{interval / 60} min")
+    logger.info("randomize: #{randomize}")
 
-    members_loop_index = 0
+    mob_members.shuffle! if randomize
+    members_list = build_members_list(mob_members)
 
-    loop do
-      current_member = mob_members[members_loop_index]
-      if members_loop_index == mob_members.count - 1
-        members_loop_index = 0
-      else
-        members_loop_index += 1
-      end
-
+    members_list.each.with_index do |current_member, i|
       notify_member_turn(current_member: current_member)
+      sleep(interval - pre_notify)
 
-      sleep(interval)
+      next_member = members_list[i + 1]
+      pre_notify_member_turn(next_member: next_member)
+
+      sleep(pre_notify)
     end
   end
 
   private
 
+  def build_members_list(mob_members)
+    [].tap do |results|
+      100.times.each do
+        mob_members.each do |m|
+          results.push m
+        end
+      end
+    end
+  end
+
+  def pre_notify_member_turn(next_member:)
+    comment = "もうすぐ <@#{next_member}> さんのターンです"
+    notifier.post_message(comment: comment)
+
+    logger.info(comment)
+  end
+
   def notify_member_turn(current_member:)
     comment = "<@#{current_member}> さんのターン！"
-    # notifier.post_message(comment: comment)
+    notifier.post_message(comment: comment)
 
     logger.info(comment)
   end
--- main.rb
+++ main.rb
@@ -8,13 +8,15 @@ class CliApp < Thor
   desc 'start mobtimer', 'start mobtimer'
   option :members, aliases: 'm', type: :string
   option :interval, aliases: 'i'
+  option :random, aliases: 'r', type: :boolean, default: false
   def mobtimer
     Dotenv.load
 
     mob_members = options[:members].split(',').map(&:strip)
     interval = options[:interval].to_i
+    randomize = options[:random] || false
 
-    MobTimer.new.start(mob_members: mob_members, interval: interval)
+    MobTimer.new.start(mob_members: mob_members, interval: interval, randomize: randomize)
   end
 end
 
